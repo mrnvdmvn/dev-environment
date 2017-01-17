9a6ede70c136cea7a14b22933296b90854e4c39f
mrnvdmvn <mrnvdmvn@outlook.com>
2017-01-17 22:08:55 +0200

    mocking http; mock api schema data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import {getUsers} from './api/userApi';
+import'./index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
 //populate table of users via Api call
 getUsers().then(result => {
@@ -14,4 +15,19 @@ getUsers().then(result => {
       </tr>`
   });
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  //must use array.from to create a real array from a DOM collection
+  //getElementsByClassName only returns an "array like" object
+
+Array.from(deleteLinks, link => {
+  link.onclick = function(event){
+    const element = event.target;
+    event.preventDefault();
+    deleteUser(element.attributes["data-id"].value);
+    const row = element.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+  };
+});
 });
