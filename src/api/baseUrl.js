1ea5118baceca63edfb5f652c29b50619b9c2554
mrnvdmvn <mrnvdmvn@outlook.com>
2017-01-18 21:31:38 +0200

    deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://powerful-fortress-73785.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url){
