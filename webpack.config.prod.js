a5ba3918bf42700748a72fb9b27203ded85dd6ca
mrnvdmvn <mrnvdmvn@outlook.com>
2017-01-18 16:21:06 +0200

    dymanic http generation

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,6 +16,22 @@ export default {
     filename: 'bundle.js' //simulate existing bundle in src dir
   },
   plugins: [
+    //create html file that includes reference to bundled.js
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true
+        },
+        inject:true
+    }),
+
     //eliminate duplicate packages when generating bundling
     new webpack.optimize.DedupePlugin(),
 
